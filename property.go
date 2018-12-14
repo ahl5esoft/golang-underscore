ef9ebfa551284d0005712f0c6567c8feadafb510
ahl5esoft <82205764@qq.com>
2018-12-14 10:10:05 +0800

    fix nested struct or ptr struct can not work

--- property.go
+++ property.go
@@ -2,33 +2,53 @@ package underscore
 
 import (
 	"errors"
+	"fmt"
 	"reflect"
 	"strings"
 )
 
+// GetProeprtyRVFunc is get property reflect.Value func
+type GetProeprtyRVFunc func(interface{}) (reflect.Value, error)
+
 // PropertyRV is 获取reflect.Value
-func PropertyRV(name string) func(interface{}) (reflect.Value, error) {
-	return func(item interface{}) (reflect.Value, error) {
-		var itemRV reflect.Value
-		if reflect.TypeOf(item) == NullRvOfRt {
-			itemRV = item.(reflect.Value)
-		} else {
-			itemRV = reflect.ValueOf(item)
-		}
-
-		if itemRV.Kind() == reflect.Ptr {
-			itemRV = itemRV.Elem()
-		}
-
+func PropertyRV(name string) GetProeprtyRVFunc {
+	var getter GetProeprtyRVFunc
+	getter = func(item interface{}) (reflect.Value, error) {
+		itemRV := getRV(item)
 		itemRT := itemRV.Type()
 		for i := 0; i < itemRT.NumField(); i++ {
-			if strings.ToLower(name) == strings.ToLower(itemRT.Field(i).Name) {
+			field := itemRT.Field(i)
+			if field.Anonymous {
+				return getter(
+					itemRV.Field(i),
+				)
+			}
+
+			if strings.ToLower(name) == strings.ToLower(field.Name) {
+				fmt.Println("nested", field, field.Anonymous)
+				if field.Anonymous {
+					fmt.Println("nested", field)
+				}
+
 				return itemRV.Field(i), nil
 			}
 		}
 
 		return NullRv, errors.New("invalid field: [" + name + "]")
 	}
+	return getter
+}
+
+func getRV(v interface{}) reflect.Value {
+	if reflect.TypeOf(v) == NullRvOfRt {
+		return v.(reflect.Value)
+	}
+
+	rv := reflect.ValueOf(v)
+	if rv.Kind() == reflect.Ptr {
+		rv = rv.Elem()
+	}
+	return rv
 }
 
 // Property is 获取属性函数
