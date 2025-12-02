4c8d6329c0136c2c609fb10cf095ac7b879b7791
ahl5esoft <82205764@qq.com>
2025-12-02 19:43:29 +0800

    CN.1875084792839892:699d29e5c3139b2c78a122f205653347_692eb5bd4e3c2243f51002eb.692eb9124e3c2243f5100431.692eb9124901274f8a90ef36:Trae CN.T(2025/12/2 18:01:54)

--- order.go
+++ order.go
@@ -2,13 +2,13 @@ package underscore
 
 import "sort"
 
-func (m enumerable) Order(selector interface{}) IEnumerable {
+func (m enumerable) Order(selectors ...interface{}) IEnumerable {
 	return enumerable{
 		Enumerator: func() IEnumerator {
 			s := new(sorter)
 			s.Sort(
 				m.GetEnumerator(),
-				selector,
+				selectors...,
 			)
 			sort.Sort(s)
 			return chainFromValue(s.ValuesValue).GetEnumerator()
@@ -16,11 +16,15 @@ func (m enumerable) Order(selector interface{}) IEnumerable {
 	}
 }
 
-func (m enumerable) OrderBy(fieldName string) IEnumerable {
-	getter := FieldValue(fieldName)
-	return m.Order(func(value, _ interface{}) facade {
-		return facade{
-			getter(value),
+func (m enumerable) OrderBy(fieldNames ...string) IEnumerable {
+	selectors := make([]interface{}, len(fieldNames))
+	for i, fieldName := range fieldNames {
+		getter := FieldValue(fieldName)
+		selectors[i] = func(value, _ interface{}) facade {
+			return facade{
+				getter(value),
+			}
 		}
-	})
+	}
+	return m.Order(selectors...)
 }
